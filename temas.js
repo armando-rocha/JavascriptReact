64e07b0facd8650e6e2ae480d9e81ad25cf9c79c
LUIS ADELFO LOBATO CAZARES <armando.rocha@elektra.com.mx>
2023-05-03 17:36:31 -0600

    inicializacion curso

---
+++ temas.js
@@ -0,0 +1,33 @@
+//alert("Holi")
+var a = 1;
+console.log(a + " variable")
+
+//anteriormente se ocupaba var
+var  producto = "String ";
+//AHORA SE OCUPA let
+let variableSinInicializar;
+let variableLet="hf31s";
+
+console.log("variableSinInicializar + "+ variableSinInicializar)
+
+
+//tipo de variables let , const
+
+const nuevaVarConst=124;
+let variableDos;
+let variableletDos="hf31s";
+
+console.log("variableSinInicializar + "+ variableSinInicializar)
+
+//La variable es mutable
+
+
+
+/*
+CONSTANTES
+*/ 
+
+
+const constante = 100;
+
+console.log(constante);
