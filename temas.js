e8849b2e7b36119117e0ef3de54eee419ae40ff9
LUIS ADELFO LOBATO CAZARES <armando.rocha@elektra.com.mx>
2023-05-03 19:07:56 -0600

    Variables

--- temas.js
+++ temas.js
@@ -8,7 +8,7 @@ var  producto = "String ";
 let variableSinInicializar;
 let variableLet="hf31s";
 
-console.log("variableSinInicializar + "+ variableSinInicializar)
+console.log("variableSinInicializar + "+ variableSinInicializar);
 
 
 //tipo de variables let , const
@@ -17,17 +17,50 @@ const nuevaVarConst=124;
 let variableDos;
 let variableletDos="hf31s";
 
-console.log("variableSinInicializar + "+ variableSinInicializar)
+console.log("variableletDos + "+ variableletDos);
+
+console.log("variableSinInicializar + "+ variableSinInicializar);
+
+variableletDos="mutada";
+
+console.log("variableletDos Mutada + "+ variableletDos);
 
 //La variable es mutable
-
 
 
 /*
 CONSTANTES
 */ 
-
-
+//La constante es como en matematicas siempre tiene un valor y es unico y no se puede modificar, se debe definir la variable.
 const constante = 100;
-
 console.log(constante);
+
+
+
+/*
+Tipos de datos
+*/
+
+//undefined
+let estudiante;
+console.log(estudiante);
+console.log(typeof(estudiante));
+
+//boolean
+let siNo=true;
+console.log(siNo);
+console.log(typeof(siNo));
+
+//numero
+let numero=-100;
+console.log(numero);
+console.log(typeof(numero));
+
+//numeroDecimal
+let numeroDecimal=100.91;
+let numeroConDecimal=numeroDecimal.toFixed(2);
+
+console.log(numeroConDecimal);
+console.log(typeof(numeroConDecimal));
+
+
